75f5111124ef1ae8838551ae8016d5a2f41117be
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-26 08:15:52 +0700

    ok

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-
-
-document.addEventListener('click', function(event) {
-    let x = event.clientX; // Posisi X (horizontal) dari klik
-    let y = event.clientY; // Posisi Y (vertikal) dari klik
-    
-    console.log('Posisi klik:', x, y);
-});
-
-  
+document.addEventListener('click', tapClick);
 document.addEventListener('touchstart', tapClick);
+document.addEventListener('touchmove', tapClick);
 
-document.addEventListener('touchmove',tapClick);
-  
-function tapClick(event){
-    if (event.touches.length > 0) {
-    console.log('Jumlah sentuhan aktif:', event.touches.length);
-    console.log(event.touches);
+function tapClick(event) {
+    // Mengecek apakah ini event sentuhan
+    if (event.touches && event.touches.length > 0) {
+        console.log('Jumlah sentuhan aktif:', event.touches.length);
+        console.log(event.touches);
     }
+    
+    // Mengecek apakah ini event mouse (click)
     if (event.clientX !== null && event.clientX !== undefined) {
         console.log('clientX memiliki nilai:', event.clientX);
-        console.log(event.clientY);
+        console.log('clientY memiliki nilai:', event.clientY);
     }
-    
 }
