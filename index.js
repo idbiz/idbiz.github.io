a21466890c6b950f6a8198dc386e91e70c7407f1
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-26 08:23:16 +0700

    ok

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener('touchstart', tapClick);
 document.addEventListener('touchmove', tapClick);
 
 function tapClick(event) {
+    let targetElement = event.target;  // Elemen yang diklik atau disentuh
+    console.log('Elemen yang dipicu:', targetElement);
     // Mengecek apakah ini event sentuhan
     if (event.touches && event.touches.length > 0) {
         console.log('Jumlah sentuhan aktif:', event.touches.length);
