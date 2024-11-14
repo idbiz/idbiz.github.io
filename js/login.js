8d681d39e12bed8fd42351389e80f8642799ae48
barganakukuhraditya <barganakukuh@gmail.com>
2024-11-14 08:59:34 +0700

    add event listeners for registration and login buttons

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const regisButton =document.getElementById('register');
+const loginButton =document.getElementById('login');
+const container = document.getElementById('container');
+
+regisButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
