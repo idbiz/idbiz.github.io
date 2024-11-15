4ee760575557d034a7928071f0b556e33b21f76f
GilangAndhika <gryzlegrizz@gmail.com>
2024-11-15 19:27:48 +0700

    Refactor back button styles

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 const regisButton =document.getElementById('register');
 const loginButton =document.getElementById('login');
 const container = document.getElementById('container');
+const backButton = document.getElementById('back-btn');
 
 regisButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
@@ -8,4 +9,8 @@ regisButton.addEventListener('click', () => {
 
 loginButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
+});
+
+backButton.addEventListener('click', () => {
+    window.location.href = '../index.html';
 });
